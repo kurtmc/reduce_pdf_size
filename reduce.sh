a0e7a19fa8adb8ccc178ca95ed5cf4e9373eca8f
Kurt McAlpine <kurt@linux.com>
2015-08-28 13:34:27 +1200

    Added command line arg parsing

--- reduce.sh
+++ reduce.sh
@@ -1,20 +1,71 @@
 #!/bin/sh
 
+function print_help {
+echo "Use -bw to convert to black and white"
+echo "Use -res XX to set the resolution default 144"
+}
+
 res=144
 
-gs  -q -dNOPAUSE -dBATCH -dSAFER \
-	-sDEVICE=pdfwrite \
-	-sColorConversionStrategy=Gray \
-	-dProcessColorModel=/DeviceGray \
-	-dCompatibilityLevel=1.3 \
-	-dPDFSETTINGS=/screen \
-	-dEmbedAllFonts=true \
-	-dSubsetFonts=true \
-	-dColorImageDownsampleType=/Bicubic \
-	-dColorImageResolution=$res \
-	-dGrayImageDownsampleType=/Bicubic \
-	-dGrayImageResolution=$res \
-	-dMonoImageDownsampleType=/Bicubic \
-	-dMonoImageResolution=$res \
-	-sOutputFile=out.pdf \
-	$1
+while [[ $# > 1 ]]
+do
+	key="$1"
+
+	case $key in
+		-res|--resolution)
+			res=$2
+			shift
+			;;
+		-bw|--blackandwhite)
+			BLACK_AND_WHITE=YES
+			shift # past argument
+			;;
+		-h|--help)
+			print_help
+			exit 0
+			shift # past argument
+			;;
+		*)
+			print_help
+			exit 1
+			# unknown option
+			;;
+	esac
+	shift # past argument or value
+done
+
+if [ "xx$BLACK_AND_WHITE" = "xxYES"]; then
+
+	gs  -q -dNOPAUSE -dBATCH -dSAFER \
+		-sDEVICE=pdfwrite \
+		-sColorConversionStrategy=Gray \
+		-dProcessColorModel=/DeviceGray \
+		-dCompatibilityLevel=1.3 \
+		-dPDFSETTINGS=/screen \
+		-dEmbedAllFonts=true \
+		-dSubsetFonts=true \
+		-dColorImageDownsampleType=/Bicubic \
+		-dColorImageResolution=$res \
+		-dGrayImageDownsampleType=/Bicubic \
+		-dGrayImageResolution=$res \
+		-dMonoImageDownsampleType=/Bicubic \
+		-dMonoImageResolution=$res \
+		-sOutputFile=out.pdf \
+		$1
+else
+	gs  -q -dNOPAUSE -dBATCH -dSAFER \
+		-sDEVICE=pdfwrite \
+		-dCompatibilityLevel=1.3 \
+		-dPDFSETTINGS=/screen \
+		-dEmbedAllFonts=true \
+		-dSubsetFonts=true \
+		-dColorImageDownsampleType=/Bicubic \
+		-dColorImageResolution=$res \
+		-dGrayImageDownsampleType=/Bicubic \
+		-dGrayImageResolution=$res \
+		-dMonoImageDownsampleType=/Bicubic \
+		-dMonoImageResolution=$res \
+		-sOutputFile=out.pdf \
+		$1
+
+fi
